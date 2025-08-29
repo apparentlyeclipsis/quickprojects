9f1ef42a1ca12639c9c86453f552c9a94e25311b
Eclipsis <xcoffee18@gmail.com>
2025-08-29 12:49:01 -0400

    Create morse-converter.lua

---
+++ lua/morse-converter.lua
@@ -0,0 +1,85 @@
+-- testing value to make sure it works
+local inputMorse = "- .... . / ...-- .-. -.. / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / ..--- / .-.. .- --.. -.-- / -.. --- --. ..."
+
+-- helpers
+local convertData = {
+  ['a'] = ".-",
+  ['b'] = "-...",
+  ['c'] = "-.-.",
+  ['d'] = "-..",
+  ['e'] = ".",
+  ['f'] = "..-.",
+  ['g'] = "--.",
+  ['h'] = "....",
+  ['i'] = "..",
+  ['j'] = ".---",
+  ['k'] = "-.-",
+  ['l'] = ".-..",
+  ['m'] = "--",
+  ['n'] = "-.",
+  ['o'] = "---",
+  ['p'] = ".--.",
+  ['q'] = "--.-",
+  ['r'] = ".-.",
+  ['s'] = "...",
+  ['t'] = "-",
+  ['u'] = "..-",
+  ['v'] = "...-",
+  ['w'] = ".--",
+  ['x'] = "-..-",
+  ['y'] = "-.--",
+  ['z'] = "--..",
+  
+  ['1'] = ".----",
+  ['2'] = "..---",
+  ['3'] = "...--",
+  ['4'] = "....-",
+  ['5'] = ".....",
+  ['6'] = "-....",
+  ['7'] = "--...",
+  ['8'] = "---..",
+  ['9'] = "----.",
+  ['10'] = "-----",
+  
+  [' '] = "/"
+}
+
+function split(str)
+  local substrs = {}
+  -- "%S+" splits along whitespace
+  for v in string.gmatch(str, "%S+") do
+    table.insert(substrs, v)
+  end
+  return substrs
+end
+
+function search(searchTable, toSearch)
+  for index, item in pairs(searchTable) do
+    if item == toSearch then
+      return index
+    end
+  end
+  
+  return "~"
+end
+
+-- morse to english
+function morseToEnglish(morseString)
+  local returnString = ""
+  local seperated = split(morseString, " ")
+  
+  for i=1, #seperated do
+    returnString = returnString .. search(convertData, seperated[i])
+  end
+  
+  return returnString
+end
+
+-- test statements
+print('input (english):')
+print('The 3rd quick brown fox jumps over 2 lazy dogs')
+print('input (morse):')
+print(inputMorse)
+print()
+print('output:')
+print(morseToEnglish(inputMorse))
